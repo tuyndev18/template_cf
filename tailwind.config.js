e2e01b9b863d6d6c92f56628308a22cc5fda6ee6
Pham Ngoc Tuyen <tuyen197.it@gmail.com>
2023-03-27 16:04:16 +0700

    style: create landing home page

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,36 @@
 module.exports = {
   content: ["./**/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        main: "#161D22",
+        white: "#FFFFFF",
+        orange: {
+          400: "#F75014",
+          500: "#CE5704",
+          600: "#F2261C",
+          700: "#F04E15"
+        },
+        green: {
+          200: "#90EE90",
+          300: "#50B81F",
+        },
+        blue: {
+          300: "#4682B4",
+          400: "#117EEC",
+          500: "#1882FF"
+        },
+        yellow: {
+          500: "#FAA514",
+        },
+        gray: {
+          300: "#6B798B",
+          400: "#DADBDE",
+          500: "#303540",
+          600: "#454D60"
+        },
+      },
+    },
   },
   plugins: [],
-}
+};
